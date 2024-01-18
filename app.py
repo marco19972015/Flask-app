f9bd3ac5069d8386d7784cea7cf3480cfd36c704
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-18 12:48:04 -0500

    In this version I created a form in index.html that redirects the data intered into the greet template. This path is provided in the action, the method get gets the data when the button is submitted. In app.py I create the new route that will route the form to the new template. I return the template path, and the second argument I set the name to the name that is returned from the request. If name isn't returned then there is a default

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ def index():
     # Instead of using if else block, I can use 'get' method
     # First argument is what value we want to get
     # Second value is what value we want the default as
-    name = request.args.get("name", "World")
+    # name = request.args.get("name", "World")
 
     # Remember for render_template to run I need a tempalte folder (DUH)
         # place holder referes to the literal placehold in the template
         # name refers to the variable above (but can be any value)
-    return render_template("index.html", name=name) 
+    return render_template("index.html") 
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
