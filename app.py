51a3372c50f11b9766815adaa8027d82b45518b0
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-18 19:18:02 -0500

    In this version I changed the form method from a GET request to a POST request in order to hide the values of the name in the URL, This caused the application to break. To fix this back in the app.py I originally add POST to the methods array. However I can also optionally combine both the GET AND POST request in the index function. Since GET is default, I can return the index page (This would happen upon refresh of page) and when we submit the form we send a POST request. Note that in this case to send the name over to the greet.html template we have to use request.form instead of request.args since args only works with the GET request.

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # This code decides based on the browsers request what file or files we will send from the server to the browser
-@app.route("/")
+# method now says we can handle both GET and POST request
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "GET": 
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
+    
     # Instead of using if else block, I can use 'get' method
     # First argument is what value we want to get
     # Second value is what value we want the default as
@@ -14,8 +20,20 @@ def index():
     # Remember for render_template to run I need a tempalte folder (DUH)
         # place holder referes to the literal placehold in the template
         # name refers to the variable above (but can be any value)
-    return render_template("index.html") 
+    # return render_template("index.html") 
 
-@app.route("/greet")
-def greet():
-    return render_template("greet.html", name=request.args.get("name", "world"))
+# To support POST request we need to include the method as an argument and pass in an array and string POST
+# expect the data via POST
+# @app.route("/greet", methods=["POST"])
+# def greet():
+#     return render_template("greet.html", name=request.args.get("name", "world"))
+    
+
+
+
+# SIDE NOTES 
+    # request.args is only used when using GET (It's a dictionary that contains all of our key valued pairs)
+    # When using POST with Flask we need to use request.form
+
+    # GET is always the default in the browsers, only when we click on a button that has been confifured to use POST 
+    # are we actually addings things to ie. a cart
